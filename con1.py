a9e7cc1863f4ea1cb3bb02b0a5c064ffe3e21cec
omkar  kulkarni <137157967+omkarkulkarni30@users.noreply.github.com>
2023-06-27 13:58:40 +0530

    Update con1.py

--- con1.py
+++ con1.py
@@ -1,4 +1,4 @@
-G_key = int(input("Enter the value for G_key: "))
+G_key = input("Enter the value for G_key: ")
 print('G_key =', G_key)
-cr = int(input("Enter the value for cr: "))
+cr = input("Enter the value for cr: ")
 print('cr =', cr)
