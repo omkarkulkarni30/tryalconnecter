bc7e8dcab2f93bd296f2cb08a260bea32c1b838e
omkar  kulkarni <137157967+omkarkulkarni30@users.noreply.github.com>
2023-06-27 16:24:15 +0530

    Update con1.py

--- con1.py
+++ con1.py
@@ -1,4 +1,8 @@
-G_key = input("Enter the value for G_key: ")
+G_key = None
+while G_key is None:
+    try:
+        G_key = int(input("Enter the value for G_key: "))
+    except ValueError:
+        print("Invalid input. Please enter an integer.")
+
 print('G_key =', G_key)
-cr = input("Enter the value for cr: ")
-print('cr =', cr)
