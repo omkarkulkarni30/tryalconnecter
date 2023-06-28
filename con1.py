571311ce06b1d3ac51e9364b00792d3c4b6a42d7
omkar  kulkarni <137157967+omkarkulkarni30@users.noreply.github.com>
2023-06-28 13:03:12 +0530

    con1.py

--- con1.py
+++ con1.py
@@ -1,16 +1,5 @@
 import json
-
-# Read JSON file
-with open("output.json", "r") as file:
-    json_data = json.load(file)
-
-# Access values and assign to variables
-for entry in json_data:
-    userid = entry["userid"]
-    password = entry["password"]
-    id_number = entry["id"]
-
-    # Use the assigned values in your code as needed
+# Use the assigned values in your code as needed
     # For example:
     print("User ID:", userid)
     print("Password:", password)
