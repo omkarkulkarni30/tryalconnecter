679e56dacb28d50eb26211f7dd86ff349a63501b
omkar  kulkarni <137157967+omkarkulkarni30@users.noreply.github.com>
2023-06-28 12:59:08 +0530

    Update con1.py

--- con1.py
+++ con1.py
@@ -1,8 +1,23 @@
-G_key = None
-while G_key is None:
-    try:
-        G_key = int(input("Enter the value for G_key: "))
-    except ValueError:
-        print("Invalid input. Please enter an integer.")
+import json
 
-print('G_key =', G_key)
+# Read JSON file
+with open("output.json", "r") as file:
+    json_data = json.load(file)
+
+# Access values and assign to variables
+for entry in json_data:
+    userid = entry["userid"]
+    password = entry["password"]
+    id_number = entry["id"]
+
+    # Use the assigned values in your code as needed
+    # For example:
+    print("User ID:", userid)
+    print("Password:", password)
+    print("ID:", id_number)
+
+    # Assign values to variables within your GitHub code
+    # For example:
+    # github_variable_1 = userid
+    # github_variable_2 = password
+    # github_variable_3 = id_number
